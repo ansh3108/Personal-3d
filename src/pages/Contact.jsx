a0d08ffd6de95d67c5de1a9dbade91e6a473dadf
Ansh Kumar <anshkumar9686@gmail.com>
2024-12-16 19:48:44 +0530

    contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+
+  const [form, setform] = useState({name: '', email: '', message: ''})
+
+  const handleChange = () => {};
+
   return (
-    <div>Contact</div>
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text"> Get in Touch</h1>
+
+          <form
+          className="w-full flex flex-col gap-7 mt-14"
+          >
+            <label className="text-black-500 font-semibold">
+              Name
+              <input 
+                type="text"
+                name="name"
+                className="input"
+                placeholder="Ansh"
+                required
+                value={}
+              />
+            </label>
+          </form>
+
+      </div>
+    </section>
   )
 }
 
