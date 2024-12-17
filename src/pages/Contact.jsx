4c40137e41fe5a38b0fee3394c68942d93b35d92
Ansh Kumar <anshkumar9686@gmail.com>
2024-12-17 19:31:48 +0530

    errors

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.sendForm(
+    emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
@@ -30,8 +30,11 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     ).then(() => {
       setIsLoading(false);
+
+      setform({ name: '', email: '', message: '' })
     }).catch((error) => {
-      
+      setIsLoading(false);
+      console.log(error);
     })
   };
 
